908bea472503670deeeb501be9c3e12c2a66339c
nnkhoa <n.nhukhoa2209@gmail.com>
2017-11-20 21:10:49 +0700

    function for CPU-OMP calculation

--- labwork/src/labwork.cu
+++ labwork/src/labwork.cu
@@ -39,6 +39,7 @@ int main(int argc, char **argv) {
             timer.start();
             labwork.labwork1_OpenMP();
             labwork.saveOutputImage("labwork2-openmp-out.jpg");
+            printf("labwork 1 CPU-OMP ellapsed %.1fms\n", lwNum, timer.getElapsedTimeInMilliSec());
             break;
         case 2:
             labwork.labwork2_GPU();
@@ -101,7 +102,17 @@ void Labwork::labwork1_CPU() {
 }
 
 void Labwork::labwork1_OpenMP() {
-
+    int pixelCount = inputImage->width * inputImage->height;
+    outputImage = static_cast<char *>(malloc(pixelCount * 3));
+    #pragma omp parallel for 
+    for (int j = 0; j < 100; j++) {     // let's do it 100 times, otherwise it's too fast!
+        for (int i = 0; i < pixelCount; i++) {
+            outputImage[i * 3] = (char) (((int) inputImage->buffer[i * 3] + (int) inputImage->buffer[i * 3 + 1] +
+                                          (int) inputImage->buffer[i * 3 + 2]) / 3);
+            outputImage[i * 3 + 1] = outputImage[i * 3];
+            outputImage[i * 3 + 2] = outputImage[i * 3];
+        }
+    }
 }
 
 int getSPcores(cudaDeviceProp devProp) {
